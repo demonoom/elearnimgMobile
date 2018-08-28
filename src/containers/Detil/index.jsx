080b22c0068881fc949b28f40be46a260e5c3efe
noom <noom0130@foxmail.com>
2018-08-28 23:33:08 +0800

    .

--- src/containers/Detil/index.jsx
+++ src/containers/Detil/index.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
 import './style.less'
+import {CSSTransition} from 'react-transition-group'
 import Header from '../../components/Header'
 
 class Detil extends React.Component {
     constructor(props, context) {
         super(props, context);
-        this.state = {}
+        this.state = {
+            show: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({show: true})
     }
 
     render() {
 
         return (
-            <div id='detil'>
-                <Header
-                    title='详情页'
-                    ref='header'
-                />
-            </div>
+            <CSSTransition
+                in={this.state.show}
+                timeout={300}
+                classNames='translate'
+            >
+                <div id='detil'>
+                    <Header
+                        title='详情页'
+                        ref='header'
+                    />
+                </div>
+            </CSSTransition>
         )
     }
 }
