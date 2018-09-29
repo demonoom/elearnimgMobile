f3acd6cf560fd017df6cb5fbd35e23492b5fc2f2
zhaojuan <343524032@qq.com>
2018-09-29 16:00:11 +0800

    -

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -27,22 +27,34 @@ class Comment extends React.Component {
 
         return (
             <div id='comment' className={this.state.commentFlag ? 'comment_panel_enter' : ''}>
-                <div onClick={this.closePanel}>
-                    X
+                <div className="icon_close" >
+                    <i className="iconfont icon-guanbi" onClick={this.closePanel}></i>
                 </div>
                 <div>
                     <h2>撰写评论</h2>
-                    <div>课程选择:
-                        <span>1</span>
-                        <ul>
-                            <li>1</li>
-                            <li>2</li>
-                            <li>3</li>
-                        </ul>
+                    <div className="my_flex">
+                        <div className="title_color title">课程选择：</div>
+                        <div className="rightDiv selectCont">
+                            <div className="select">1</div>
+                            <ul>
+                                <li className="text_hidden">1</li>
+                                <li>2</li>
+                                <li>3</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div>课程评分:
+                    <div className="my_flex">
+                        <div className="title_color title">课程评分：</div>
+                        <div className="star rightDiv">
+                            <i className="iconfont icon-kongwujiaoxing"></i>
+                            <i className="iconfont icon-kongwujiaoxing"></i>
+                            <i className="iconfont icon-kongwujiaoxing"></i>
+                            <i className="iconfont icon-kongwujiaoxing"></i>
+                            <i className="iconfont icon-kongwujiaoxing"></i>
+                            <span className="title_color">9</span>
+                        </div>
                     </div>
-                    <textarea name="" id="" cols="30" rows="10"></textarea>
+                    <textarea name="" id="" placeholder="请输入评价内容"></textarea>
                     <Button type="primary" onClick={this.comment}>提交</Button>
                 </div>
             </div>
