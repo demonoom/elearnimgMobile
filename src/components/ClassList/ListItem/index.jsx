f8f684a3178049310fc528c578edd82425f41091
noom <noom0130@foxmail.com>
2018-11-07 14:01:38 +0800

    .

--- src/components/ClassList/ListItem/index.jsx
+++ src/components/ClassList/ListItem/index.jsx
@@ -52,7 +52,7 @@ class ListItem extends React.Component {
                             style={{display: listType === '1' ? itemObj.courseStatus === '3' ? 'none' : arr.indexOf('2') === -1 ? '' : 'none' : 'none'}}>
                              开课时间：
                             {
-                                FormatTime.getLastLivTime(itemObj)
+                                FormatTime.getLastLivTime(itemObj) || FormatTime.formatTime(itemObj.startTime)
                             }
                         </span>
                         <span
